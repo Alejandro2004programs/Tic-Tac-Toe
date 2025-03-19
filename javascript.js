0e7191410fb061f22df4ff1ac053937c8158b0fb
Alejandro Alvarez <ala007@ucsd.edu>
2025-03-18 23:33:45 -0700

    wrote checkwin and checktie functions

--- javascript.js
+++ javascript.js
@@ -14,24 +14,55 @@ const GameController = (function() {
     const makeMove = function(row, column, value) {
         if(board[row][column] == "") {
             board[row][column] = value;
+            console.log(board);
+            return true;
         }
         console.log(board);
+        return false;
     }
     const checkWin = function() {
-        let hasWon;
+        let hasWon = false;
         // Check win logic here
+        for(let i = 0; i < 3; i++) {
+            if(board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+            if(board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return true;
+            }
+        }
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true;
+        }
+        if(board[0][2] == board[1][1] && board[1][1] == board[3][0]) {
+            return true;
+        }
         return hasWon;
+    }
+    const checkTie = function() {
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                if(board[i][j] == "") {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
     const PlayRound = function() {
         while(checkWin == False) {
 
         }
-        
+        clearBoard();
     }
-    const clearBoard= function() {
-
+    const clearBoard = function() {
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                board[i][j] == "";
+            }
+        }
     }
-    return {board, makeMove, checkWin, PlayRound, clearBoard};
+    return {board, makeMove, checkWin, checkTie, PlayRound, clearBoard};
 }) ();
 
 
