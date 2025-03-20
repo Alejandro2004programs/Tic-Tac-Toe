df94b5eabaa80698f274b60abc52e78ff927401e
Alejandro Alvarez <ala007@ucsd.edu>
2025-03-19 20:51:16 -0700

    coded some more for the game to work in the console. have to debug checkWin function and checkTie functions

--- javascript.js
+++ javascript.js
@@ -1,10 +1,3 @@
-function createPlayer(value) {
-    return {value};
-}
-
-Player1 = createPlayer("X");
-Player2 = createPlayer("O");
-
 const GameController = (function() {
     const board = [
     ["", "", ""], 
@@ -12,35 +5,28 @@ const GameController = (function() {
     ["", "", ""]
     ];
     const makeMove = function(row, column, value) {
-        if(board[row][column] == "") {
-            board[row][column] = value;
-            console.log(board);
-            return true;
-        }
+        board[row][column] = value;
         console.log(board);
-        return false;
     }
     const checkWin = function() {
-        let hasWon = false;
-        // Check win logic here
-        for(let i = 0; i < 3; i++) {
-            if(board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+        for(let i = 0; i <= 2; i++) {
+            if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != "") {
                 return true;
             }
-            if(board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+            if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[i][0] != "") {
                 return true;
             }
         }
-        if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != "") {
             return true;
         }
-        if(board[0][2] == board[1][1] && board[1][1] == board[3][0]) {
+        if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][0] != "") {
             return true;
         }
-        return hasWon;
+        return false;
     }
     const checkTie = function() {
-        for(let i = 0; i < 3; i++) {
+        for(let i = 0; i <= 2; i++) {
             for(let j = 0; j < 3; j++) {
                 if(board[i][j] == "") {
                     return false;
@@ -49,12 +35,6 @@ const GameController = (function() {
         }
         return true;
     }
-    const PlayRound = function() {
-        while(checkWin == False) {
-
-        }
-        clearBoard();
-    }
     const clearBoard = function() {
         for(let i = 0; i < 3; i++) {
             for(let j = 0; j < 3; j++) {
@@ -62,9 +42,71 @@ const GameController = (function() {
             }
         }
     }
-    return {board, makeMove, checkWin, checkTie, PlayRound, clearBoard};
+    const isLegalMove = function(row, col) {
+        if(row > 2 || row < 0) {
+            return false;
+        }
+        if(col > 2 || col < 0) {
+            return false;
+        }
+        if(board[row][col] == "") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    return {board, makeMove, checkWin, checkTie, clearBoard, isLegalMove};
 }) ();
 
 
+function createPlayer(value) {
+    return {value};
+}
+
+Player1 = createPlayer("X");
+Player2 = createPlayer("O");
 
 
+// Note: after it is working, I should try to put this function inside of the GameController to further encapsulate my code,
+// I should have that be the playRound() function. 
+function startGame() {
+    let X = Player1.value;
+    let O = Player2.value;
+    while(GameController.checkWin() == false && GameController.checkTie() == false) {
+        let row = parseInt(prompt("Row: "));
+        let col = parseInt(prompt("Col: "));
+        while(GameController.isLegalMove(row, col) == false) {
+            row = parseInt(prompt("Please select a valid row: "));
+            col = parseInt(prompt("Please select a valid column: "));
+        }
+        GameController.makeMove(row, col, X);
+        if(GameController.checkWin() == true) {
+            console.log("Tie");
+            break;
+        }
+        else if(GameController.checkTie() == true) {
+            console.log("X has won");
+            break;
+        }
+        console.log(GameController.board);
+        row = prompt("Row: ");
+        col = prompt("Col: ");
+        while(GameController.isLegalMove(row, col) == false) {
+            row = parseInt(prompt("Please select a valid row: "));
+            col = parseInt(prompt("Please select a valid column: "));
+        }
+        GameController.makeMove(row, col, O);
+        if(GameController.checkTie() == true) {
+            console.log("Tie");
+            break;
+        }
+        else if(GameController.checkWin() == true) {
+            console.log("O has won");
+            break;
+        }
+    }
+    GameController.clearBoard();
+}
+
+startGame();
