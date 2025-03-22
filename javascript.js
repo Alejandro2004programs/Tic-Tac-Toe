25fbf43cd1d29bfd5b6fc1ae98d80916f770dcd2
Alejandro Alvarez <ala007@ucsd.edu>
2025-03-21 20:26:40 -0700

    game works for a single game

--- javascript.js
+++ javascript.js
@@ -14,14 +14,13 @@ const GameController = (function() {
     ];
     const makeMove = function(row, column, value) {
         board[row][column] = value;
-        console.log(board);
     }
     const checkWin = function() {
         for(let i = 0; i <= 2; i++) {
             if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != "") {
                 return true;
             }
-            if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[i][0] != "") {
+            if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != "") {
                 return true;
             }
         }
@@ -67,48 +66,13 @@ const GameController = (function() {
             return false;
         }
     }
-    const playRound = function() {
-        let X = Player1.value;
-        let O = Player2.value;
-        while(checkWin() == false && checkTie() == false) {
-            let row = parseInt(prompt("Row: "));
-            let col = parseInt(prompt("Col: "));
-            while(isLegalMove(row, col) == false) {
-                row = parseInt(prompt("Please select a valid row: "));
-                col = parseInt(prompt("Please select a valid column: "));
-            }
-            makeMove(row, col, X);
-            if(checkWin() == true) {
-                console.log("Tie");
-                break;
-            }
-            else if(checkTie() == true) {
-                console.log("X has won");
-                break;
-            }
-            row = prompt("Row: ");
-            col = prompt("Col: ");
-            while(isLegalMove(row, col) == false) {
-                row = parseInt(prompt("Please select a valid row: "));
-                col = parseInt(prompt("Please select a valid column: "));
-            }
-            makeMove(row, col, O);
-            if(checkTie() == true) {
-                console.log("Tie");
-                break;
-            }
-            else if(checkWin() == true) {
-                console.log("O has won");
-                break;
-            }
-        }
-        clearBoard();
-    }
-    return {board, makeMove, checkWin, checkTie, clearBoard, isLegalMove, playRound};
+    return {board, makeMove, checkWin, checkTie, clearBoard, isLegalMove};
 }) ();
 
 
 const displayGame = (function() {
+    let X = Player1.value;
+    let O = Player2.value;
     let turn = 1;
     const nextTurn = function() {
         turn++;
@@ -122,6 +86,7 @@ const displayGame = (function() {
             for(let j = 0; j < 3; j++) {
                 let square = document.createElement("p");
                 square.setAttribute("class", "square");
+                square.setAttribute("data-index-number", i.toString() + j.toString());
                 square.textContent = GameController.board[i][j];
                 displayRow.appendChild(square);
             }
@@ -129,18 +94,45 @@ const displayGame = (function() {
     }
     const addClickEvents = function() {
         const squares = document.querySelectorAll(".square");
+        const gameStatus = document.querySelector(".gameStatusText");
         squares.forEach((square) => {
             square.addEventListener("click", () => {
-                if(square.textContent == "" && (turn % 2) == 1) {
-                    square.textContent = "X";
-                    turn++;
-                }
-                else if(square.textContent == "" && (turn % 2) == 0) {
-                    square.textContent = "O";
-                    turn++;
-                }
+                if(GameController.checkWin() == false && GameController.checkTie() == false) {
+                    let i = (square.getAttribute("data-index-number"))[0];
+                    let j = (square.getAttribute("data-index-number"))[1];
+                    if(GameController.isLegalMove(i, j) == true && (turn % 2) == 1) {
+                        GameController.makeMove(i, j, X);
+                        square.textContent = X;
+                        if(GameController.checkWin() == true) {
+                            gameStatus.textContent = "X has won";
+                        }
+                        else if(GameController.checkTie() == true) {
+                            gameStatus.textContent = "It's a tie";
+                        }
+                        else {
+                            gameStatus.textContent = "O to move";
+                        }
+                        nextTurn();
+                    }
+                    else if(GameController.isLegalMove(i, j) && (turn % 2) == 0) {
+                        GameController.makeMove(i, j, O);
+                        square.textContent = O;
+                        if(GameController.checkWin() == true) {
+                            gameStatus.textContent = "O has won";
+                        }
+                        else if(GameController.checkTie() == true) {
+                            gameStatus.textContent = "It's a tie";
+                        }
+                        else {
+                            gameStatus.textContent = "X to move";
+                        }                        
+                        nextTurn();
+                    }
+                
+                }   
             });
         });
+
     }
     return {renderContents, addClickEvents, nextTurn};
 }) ();
